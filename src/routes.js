e24bd06c68028fe6c4abdefb90845224ab4e2e52
Parth Chandna <pchandna24@gmail.com>
2019-06-17 01:09:29 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,12 @@ import React , { Component } from 'react';
 import {Route,Switch} from 'react-router-dom';
 
 class Routes extend Component {
-    
+    render(){
+        return(
+            <Switch>
+                
+            </Switch>
+        )
+    }
+
 }
