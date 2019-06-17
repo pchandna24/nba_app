4fb2380b702cba9784d8914d264344567d9234cb
Parth Chandna <pchandna24@gmail.com>
2019-06-18 00:59:16 +0530

    sidenav

---
+++ src/components/Header/SideNav/sideNav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import SideNav from 'react-simple-sidenav';
+
+
+
+
+const SideNavigation = (props) =>{
+    return(
+        <div>
+            <SideNav
+            showNav={props.showNav}
+            onHideNav={props.onHideNav}
+            navStyle={{
+                background:'#242424',
+                maxWidth:'250px'
+            }}
+            
+            
+            >
+                options
+            </SideNav>
+        </div>
+    )
+}
+
+
+export default SideNavigation;
---
+++ src/components/Header/header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
+
+import SideNav from './SideNav/sideNav';
+
+
+
+
+const Header = (props) =>{
+
+
+
+
+    const navBars = () =>(
+        <div className="bars">
+            <FontAwesome name="bars"
+            onClick={props.onOpenNav}
+            style={{
+                color:'#dfdfdf',
+                padding:'10px',
+                cursor:'pointer'
+                
+            }}
+            
+            />
+        </div>
+    )
+
+    const logo = () =>{
+        return(
+            <Link to="/" className="logo">
+                <img alt="nba logo" src="/images/nba_logo.png"/>  
+            </Link>
+        )
+    }
+
+
+
+
+    return(
+        <header className="header">
+            <SideNav {...props}/>
+            <div className="headerOpt">
+                {navBars()}
+                {logo()}
+            </div>
+            
+        </header>
+    )
+}
+
+export default Header; 
