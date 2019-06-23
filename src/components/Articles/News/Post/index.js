1090f3940add8d3984940672f504d4263ac4a54e
Parth Chandna <pchandna24@gmail.com>
2019-06-24 02:03:54 +0530

    article view

---
+++ src/components/Articles/News/Post/index.js
@@ -0,0 +1,16 @@
+import React , { Component } from 'react';
+import axios from 'axios';
+import { URL } from '../../../../config';
+
+class NewsArticles extends Component {
+    render(){
+        return(
+            <div>
+                article view
+            </div>
+        )
+
+    }
+}
+
+export default NewsArticles;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch} from 'react-router-dom';
 
 import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
+import NewsArticle from './components/Articles/News/Post/index';
 
 
 class Routes extends Component {
@@ -10,7 +11,8 @@ class Routes extends Component {
         return(
             <Layout>
                  <Switch>
-                    <Route path="/" exact component={Home}/>   
+                    <Route path="/" exact component={Home}/> 
+                    <Route path="/articles/:id" exact component ={NewsArticle}/>
                 </Switch>
             </Layout>
            
