0da8d4ba9056596c321f92e0ad0e5fc9db497875
Parth Chandna <pchandna24@gmail.com>
2019-06-24 00:21:17 +0530

    video templates

--- src/components/widgets/VideosList/videosListTemplate.js
+++ src/components/widgets/VideosList/videosListTemplate.js
@@ -1,49 +1,49 @@
 import React from 'react';
+import styles from './video.css';
+
 import { Link } from 'react-router-dom';
-
 import CardInfo from '../CardInfo/cardinfo';
-import styles from '../VideosList/video.css'
 
-const VideosListTemplate = (props) =>{
-    
-    return props.data.map((item,i)=>(
-         <Link to={`/videos/${item.id}`} key={i}>
+const VideosListTemplate = (props) => {
+    return props.data.map( (item,i)=>(
+        <Link to={`/videos/${item.id}`}key={i}>
             <div style={{
-                    display:'flex',
-                    borderBottom:'2px solid #d5d5d5',
-                    background:'#fff'
+                  display:'flex',
+                  borderBottom:'2px solid #d5d5d5',
+                  background: '#fff',
             }}>
-                <div style={{
-                    background:`url(/images/videos/${item.image})`,
-                    backgroundSize:'cover !important',
-                    width:'90px',
-                    backgroundSize:'40px !important',
-                    backgroundRepeat:'no-repeat !important',
-                    backgroundPosition:'center center !important',
-                    background:`url('/images/play.png')`
+                <div className={styles.left}
+                    style={{
+                        background:`url(/images/videos/${item.image})`,
+                        backgroundSize:'cover' 
+                    }}
+                
+                >
 
-                }}>
+                    <div style={{
+                        width:'90px',
+                        height:'90px',
+                        backgroundSize:'40px !important',
+                        backgroundRepeat:'no-repeat !important',
+                        backgroundPosition:'center center !important',
+                       // background:`url(/images/play.png)`
+                        
+                    }}>
+
+                    </div>
 
                 </div>
-                <div style={{
-                    flexGrow:'1',
-                    padding:'10px'
-
-                }}>
+                <div className={styles.right}>
                     <h2 style={{
-                       fontSize:'30px',
-                       color:'#525252'
-
+                        fontSize:'13px',
+                        color:'#525252'
                     }}>{item.title}</h2>
-
                 </div>
-
             </div>
         </Link>
-    ))
+    ) )
+
+
 }
 
 export default VideosListTemplate;
-
-
-
