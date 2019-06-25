2b18cc6cff5c97ced0acb0a473624130cb55811d
Parth Chandna <pchandna24@gmail.com>
2019-06-25 22:43:17 +0530

    fixing articles

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -1,6 +1,7 @@
 import React , { Component } from 'react';
 import axios from 'axios';
 import { URL } from '../../../../config';
+//import Header from './header';
 
 class NewsArticles extends Component {
 
@@ -12,28 +13,36 @@ class NewsArticles extends Component {
     componentWillMount(){
 
         axios.get(`${URL}/articles?id=${this.props.match.params.id}`)
-        .then( response => {
-            let article=response.data[0];
+        .then(
+            response => {
+            
+            let article = response.data[0];
 
             axios.get(`${URL}/teams?id=${article.team}`)
-            .then( response =>{
+            .then( response => {
                 this.setState({
                     article,
                     team:response.data
                 })
-
             })
-            
-
 
         })
+     
     }
 
     
     render(){
+        console.log(this.state);
+        const article= this.state.article;
+        const team= this.state.team;
+
+
+
         return(
-            <div>
-                article view
+            <div className={StyleSheet.articlewraaper}>
+               article view
+                
+                
             </div>
         )
 
