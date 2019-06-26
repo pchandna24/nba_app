ca53a600d9770a765009935a5ff00208b7d6f77a
Parth Chandna <pchandna24@gmail.com>
2019-06-26 23:29:35 +0530

    styling info

--- src/components/Articles/Elements/teamNfo.js
+++ src/components/Articles/Elements/teamNfo.js
@@ -2,8 +2,44 @@ import React from 'react';
 
 
 
-const teamNfo = (props) =>{
-    <div>
+const TeamNfo = (props) =>{
+    <div styles={{
+        border:'1px solid #c5c5c5',
+        borderBottom:'0px',
+        background:'#fff',
+        margin:'5px 5px 0 5px',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+    }}>
+        <div styles={{
+            background:`url('/images/teams/${props.team.logo}')`,
+            width:'57px',
+            height:'50px',
+            backgroundSize:'65% !important',
+            backgroundRepeat:'no-repeat !important',
+            backgroundPosition:'center center !important'
+
+        }}>
+
+        </div>
+        <div styles={{
+            flexGrow:'1',
+            paddingLeft:'10px',
+            fontSize:'10px',
+            fontWeight:'300'
         
+
+        }}>
+            <div>
+                <span>{props.team.city} {props.team.name}</span>
+            </div>
+            <div>
+                <strong>W{props.team.stats[0].wins}-L{props.team.stats[0].defeats}</strong>
+            </div>
+
+        </div>
     </div>
 }
+
+export default TeamNfo;
--- src/components/Articles/News/Post/header.js
+++ src/components/Articles/News/Post/header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import TeamNfo from '../../Elements/teamNfo';
 
 
 const Header = (props) =>{
+
+    const teanNfo = (team) =>{
+        return team ? (
+            <TeamNfo team={team}/> 
+
+        ):null;
+    }
+
     return(
         <div>
-            header
+            {teamNfo(props.teamData)}
         </div>
     )
 }
