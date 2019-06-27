3c174f1b1079f2e0776f00c0724621ea4057785b
Parth Chandna <pchandna24@gmail.com>
2019-06-28 02:16:21 +0530

    Update videosRelated.js

--- src/components/widgets/VideosList/VideosRelated/videosRelated.js
+++ src/components/widgets/VideosList/VideosRelated/videosRelated.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import VideoListTemplate from '../videosListTemplate';
+
 
 
 
 const VideosRelated = (props) =>{
     return(
-        <div>
-        related
+    <div style={{
+        margin:'0 5px',
+
+
+    }} >
+        <h3 style={{
+            textAlign:'center',
+            color:'#4d4d4d',
+            background:'#fff',
+            paddingTop:'10px',
+            paddingBottom:'10px',
+            marginLeft:'1px',
+            marginRight:'1px'
+
+        }}>Related Videos</h3>
+        <VideoListTemplate
+        data={props.data}
+        teams={props.teams}
+        />
     </div>
     )
 
