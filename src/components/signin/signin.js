d8935d8f8b2b77e3a8bac21d842690ab965175b7
Parth Chandna <pchandna24@gmail.com>
2019-07-12 01:02:51 +0530

    Update signin.js

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -6,11 +6,52 @@ import styles from './signin.css';
 
 class SignIn extends Component{
     state={
+        registerError:'',
+        loading:false,
+        formdata:{
+            email:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'email_input',
+                    type:'email',
+                    placeholder:'Enter your Email'
+                },
+                validation:{
+                    required:true,
+                    email:true,
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
+
+            },
+            password:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'password_input',
+                    type:'password',
+                    placeholder:'Enter your Password'
+                },
+                validation:{
+                    required:true,
+                    password:true,
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
+            }
+        }
 
     }
+
+
     render(){
         return(
-            <div>Sign in</div>
+            <div>
+
+            </div>
         )
     }
 } 
