8f50980acbe7ec2e50dbe4048e831c6961102d7b
Parth Chandna <pchandna24@gmail.com>
2019-07-12 01:20:56 +0530

    Create formFields.js

---
+++ src/components/widgets/FormFields/formFields.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './formFields.css';
+
+
+const FormFields = () =>{
+    return(
+        <div>
+            template input
+        </div>
+    )
+}
+
+
+
+export default FormFields;
