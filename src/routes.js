da0bf61a0b38b3b018c63efa0ba85d50d96c8c76
Parth Chandna <pchandna24@gmail.com>
2019-07-12 00:54:22 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
 import NewsMain from './components/Articles/News/Main/index';
 import VideosMain from './components/Articles/Videos/Main/index';
+import SignIn from './components/signin/signin';
 
 
 class Routes extends Component {
@@ -19,6 +20,7 @@ class Routes extends Component {
                     <Route path="/articles/:id" exact component ={NewsArticle}/>
                     <Route path="/videos/:id" exact component={VideoArticle}/>
                     <Route path="/videos" exact component={VideosMain}/>
+                    <Route path="/sign-in" exact component={SignIn}/>
                 </Switch>
             </Layout>
            
