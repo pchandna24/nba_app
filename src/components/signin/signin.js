48896f042605a9871f5c0567600613f17c38a476
Parth Chandna <pchandna24@gmail.com>
2019-07-12 00:42:47 +0530

    Create signin.js

---
+++ src/components/signin/signin.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import styles from './signin.css';
+ 
+
+
+
+class SignIn extends Component{
+    state={
+
+    }
+    render(){
+        return(
+            <div>Sign in</div>
+        )
+    }
+} 
+
+
+
+export default SignIn;
