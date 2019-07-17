4a77c52d4e91a6b4c8443c7dbcbd0c3ea1154348
Parth Chandna <pchandna24@gmail.com>
2019-07-18 00:16:43 +0530

    Update formFields.js

--- src/components/widgets/FormFields/formFields.js
+++ src/components/widgets/FormFields/formFields.js
@@ -8,6 +8,23 @@ import './formFields.css';
 
 const FormFields = ({formdata,change,id}) =>{
 
+    const showError =() =>{
+        let errorMessage = null;
+
+        if(formdata.validation && !formdata.valid){
+            errorMessage=(
+                <div className="labelError">
+                    {formdata.validationMessage}
+                </div>
+            )
+        }
+
+
+        return errorMessage;
+    }
+
+
+
     const renderTemplate = () => {
         let formTemplate = null;
 
@@ -22,6 +39,7 @@ const FormFields = ({formdata,change,id}) =>{
                         onChange={(event) => change({event,id,blur:false})}
 
                         />
+                        {showError()}
                     </div>
                 )
                 break;
