0379ca8a44e21475d95e2459dd095d30f578cec0
Parth Chandna <pchandna24@gmail.com>
2019-07-25 00:36:45 +0530

    Update dashboard.js

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,4 +1,5 @@
 import React , { Component } from 'react';
+import FormFields from '../widgets/FormFields/formFields';
 import './dashboard.css';
 
 
@@ -6,6 +7,45 @@ import './dashboard.css';
 class Dashboard extends Component {
 
     state={
+        postError:'',
+        loading:false,
+        formdata:{
+            author:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'author_input',
+                    type:'text',
+                    placeholder:'Enter your name'
+                },
+                validation:{
+                    required:true,
+                    
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
+
+            },
+            title:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'title_input',
+                    type:'text',
+                    placeholder:'Enter the title'
+                },
+                validation:{
+                    required:true,
+                    
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
+
+            },
+
+        }
 
     }
 
@@ -19,7 +59,11 @@ class Dashboard extends Component {
                 <form onSubmit={this.submitForm}>
                     <h2>Add Post</h2>
 
-                    dashboard
+                    <FormFields
+                        id={'author'}
+                        formdata={this.state.formdata.author}
+                        change={(element)=>this.updateForm(element)}
+                    />
 
                 </form>
             </div>
